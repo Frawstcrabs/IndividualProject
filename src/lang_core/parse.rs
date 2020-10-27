3b4821abd8f3af7732a9d18410e7adb413eb5da7
Zakariah Nuccio <k1891842@kcl.ac.uk>
2020-10-27 18:35:32 +0000

    Add root level parser

--- src/lang_core/parse.rs
+++ src/lang_core/parse.rs
@@ -1,13 +1,20 @@
 extern crate nom;
 use nom::{
     IResult,
-    multi::many0,
-    bytes::complete::{tag, take_until},
-    combinator::not,
+    multi::{many0, fold_many0},
+    bytes::complete::{tag, take_until, take_till1},
+    combinator::{not, map},
     branch::alt
 };
 
-pub fn parse_comment(input: &str) -> IResult<&str, ()> {
+#[derive(Clone, Debug)]
+pub enum AST {
+    String(String),
+    Function(Vec<Vec<AST>>),
+    Variable(Vec<Vec<AST>>)
+}
+
+fn parse_comment(input: &str) -> IResult<&str, ()> {
     let (input, _) = tag("{!")(input)?;
 
     pub fn parse_comment_str(input: &str) -> IResult<&str, ()> {
@@ -29,4 +36,27 @@ pub fn parse_comment(input: &str) -> IResult<&str, ()> {
 
     let (input, _) = tag("!}")(input)?;
     Ok((input, ()))
+}
+
+pub fn parse_base(input: &str) -> IResult<&str, Vec<AST>> {
+    fold_many0(
+        alt((
+            map(take_till1(|c| c == '{'), Some),
+            map(parse_comment, |_| None),
+        )),
+        Vec::new(),
+        |mut vec, r: Option<&str>| {
+            if let Some(s) = r {
+                match vec.last_mut() {
+                    Some(AST::String(ast_str)) => {
+                        ast_str.push_str(s);
+                    }
+                    _ => {
+                        vec.push(AST::String(s.to_owned()));
+                    }
+                }
+            }
+            vec
+        }
+    )(input)
 }
