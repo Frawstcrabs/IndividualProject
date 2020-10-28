7092cb5de8e30c6cd4745bc0bbd4dbf4d8684757
Zakariah Nuccio <k1891842@kcl.ac.uk>
2020-10-28 17:03:09 +0000

    Add parser for blocks

--- src/lang_core/parse.rs
+++ src/lang_core/parse.rs
@@ -4,7 +4,8 @@ use nom::{
     multi::{many0, fold_many0},
     bytes::complete::{tag, take_until, take_till1},
     combinator::{not, map},
-    branch::alt
+    branch::alt,
+    character::complete::char,
 };
 
 #[derive(Clone, Debug)]
@@ -38,25 +39,102 @@ fn parse_comment(input: &str) -> IResult<&str, ()> {
     Ok((input, ()))
 }
 
+fn add_block_arg(mut vec: Vec<AST>, r: Option<AST>) -> Vec<AST> {
+    if let Some(ast) = r {
+        match (&ast, vec.last_mut()) {
+            (AST::String(s), Some(AST::String(ast_str))) => {
+                ast_str.push_str(s);
+            }
+            _ => {
+                vec.push(ast);
+            }
+        }
+    }
+    vec
+}
+
+fn parse_block_arg(chars: &[char]) -> impl Fn(&str) -> IResult<&str, Vec<AST>> + '_ {
+    move |i: &str| {
+        fold_many0(
+            alt((
+                map(take_till1(|c| chars.contains(&c)), |s: &str| Some(AST::String(s.to_owned()))),
+                map(parse_comment, |_| None),
+                map(parse_block, Some)
+            )),
+            Vec::new(),
+            add_block_arg
+        )(i)
+    }
+}
+
+fn parse_block_args(mut input: &str) -> IResult<&str, (Vec<Vec<AST>>, &str)> {
+    let mut args = Vec::new();
+    loop {
+        let (i, arg) = parse_block_arg(&['{',':',';','}'])(input)?;
+        let (i, sep) = alt((
+            tag(";}"),
+            tag(";"),
+            tag("}")
+        ))(i)?;
+        args.push(arg);
+        match sep {
+            ";" => {
+                input = i;
+                continue;
+            }
+            ";}" => {
+                return Ok((i, (args, ";}")));
+            }
+            "}" => {
+                return Ok((i, (args, "}")));
+            }
+            _ => unreachable!()
+        }
+    }
+}
+
+fn parse_block(input: &str) -> IResult<&str, AST> {
+    let (input, _) = char('{')(input)?;
+    not(char('!'))(input)?;
+    not(char('>'))(input)?;
+
+    let (input, arg1) = parse_block_arg(&['{',':',';','}'])(input)?;
+
+    let (input, sep) = alt((
+        tag(":"),
+        tag(";}"),
+        tag("}"),
+        tag(";")
+    ))(input)?;
+
+    match sep {
+        ":" => {
+            let (input, (mut args, end)) = parse_block_args(input)?;
+            args.insert(0, arg1);
+            match end {
+                ";}" => Ok((input, AST::Function(args))),
+                "}"  => Ok((input, AST::Variable(args))),
+                _    => unreachable!()
+            }
+        },
+        ";}" => Ok((input, AST::Function(vec![arg1]))),
+        "}"  => Ok((input, AST::Variable(vec![arg1]))),
+        ";" => {
+            let (input, _) = parse_block_args(input)?;
+            Ok((input, AST::String(String::from("<error:missing semicolon>"))))
+        },
+        _ => unreachable!()
+    }
+}
+
 pub fn parse_base(input: &str) -> IResult<&str, Vec<AST>> {
     fold_many0(
         alt((
-            map(take_till1(|c| c == '{'), Some),
+            map(take_till1(|c| c == '{'), |s: &str| Some(AST::String(s.to_owned()))),
             map(parse_comment, |_| None),
+            map(parse_block, Some)
         )),
         Vec::new(),
-        |mut vec, r: Option<&str>| {
-            if let Some(s) = r {
-                match vec.last_mut() {
-                    Some(AST::String(ast_str)) => {
-                        ast_str.push_str(s);
-                    }
-                    _ => {
-                        vec.push(AST::String(s.to_owned()));
-                    }
-                }
-            }
-            vec
-        }
+        add_block_arg
     )(input)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ mod lang_core;
 use lang_core::parse;
 
 fn main() {
-    println!("{:?}", parse::parse_base("before{!comment{!inner!}!}after"));
+    println!("{:?}", parse::parse_base("{set:a;te{!ignored!}st;}{a}"));
 }
