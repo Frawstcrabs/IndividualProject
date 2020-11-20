0f2958e72b4d6e376162199fec30917e31a6d679
Zakariah Nuccio <k1891842@kcl.ac.uk>
2020-11-20 10:18:08 +0000

    Update main function

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ mod lang_core;
 use lang_core::{parse, bytecode, interp};
 
 fn main() {
-    let (_, ast) = parse::parse_base("{set:a;b;}text {a}").unwrap();
+    let input = "{set:a;{if:1;b;0;d;1;f;g;};}{a}";
+    println!("{:?}", input);
+    let (_, ast) = parse::parse_base(input).unwrap();
     let mut ctx = interp::Context::new();
     let program = bytecode::generate_bytecode(&ast);
-    println!("{:?}", program.instructions);
+    for (inst, i) in program.instructions.iter().zip(0..) {
+        println!("{:<2} - {:?}", i, inst);
+    }
     ctx.interpret(&program);
-    println!("{:?}", ctx.stack);
+    println!("stack: {:?}", ctx.stack);
 }
-
-
-
