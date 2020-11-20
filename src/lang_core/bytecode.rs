c7202f9ddae8c1af1171c5f3cc437b63ab7ed63e
Zakariah Nuccio <k1891842@kcl.ac.uk>
2020-11-20 10:17:34 +0000

    Implement if statements

--- src/lang_core/bytecode.rs
+++ src/lang_core/bytecode.rs
@@ -2,8 +2,10 @@ use crate::lang_core::parse::AST;
 
 #[derive(Debug)]
 pub enum Instruction {
-    PUSH(String),
+    PUSHSTR(String),
     PUSHNIL,
+    IFFALSE(usize),
+    GOTO(usize),
     CONCAT(usize),
     SETVAR,
     DEREFVAR,
@@ -25,11 +27,56 @@ fn ast_bytecode(prog: &mut Program, ast: &AST, stackvals: &mut usize) {
             match &name[..] {
                 [AST::String(s)] => match &s[..] {
                     "set" => {
-                        // set command read
                         assert_eq!(args.len(), 3);
                         ast_vec_bytecode(prog, &args[1]);
                         ast_vec_bytecode(prog, &args[2]);
                         prog.instructions.push(Instruction::SETVAR);
+                    }
+                    "if" => {
+                        assert!(args.len() > 1);
+                        *stackvals += 1;
+                        let mut i = 1;
+                        let mut end_jumps = Vec::new();
+                        let mut prev_jump: usize;
+                        while i < args.len() {
+                            ast_vec_bytecode(prog, &args[i]);
+                            if i == args.len() - 1 {
+                                // else branch, break to avoid an unnecessary jump
+                                break;
+                            }
+                            prev_jump = prog.instructions.len();
+                            prog.instructions.push(Instruction::IFFALSE(0));
+                            i += 1;
+                            ast_vec_bytecode(prog, &args[i]);
+
+                            let current_len = prog.instructions.len();
+                            end_jumps.push(current_len);
+                            prog.instructions.push(Instruction::GOTO(0));
+
+                            // correct above cond jump to point past this branch
+                            match &mut prog.instructions[prev_jump] {
+                                Instruction::IFFALSE(p) => {
+                                    // add one to skip the above jmp to end
+                                    *p = current_len+1;
+                                }
+                                _ => unreachable!()
+                            }
+                            i += 1;
+                        }
+                        if args.len() % 2 == 1 {
+                            // no else branch given, add a nil for a placeholder
+                            prog.instructions.push(Instruction::PUSHNIL);
+                        }
+                        // correct end jumps to point past all the compiled branches
+                        let current_len = prog.instructions.len();
+                        for inst in end_jumps {
+                            match &mut prog.instructions[inst] {
+                                Instruction::GOTO(p) => {
+                                    *p = current_len;
+                                }
+                                _ => unreachable!()
+                            }
+                        }
                     }
                     _ => {
                         panic!("unsupported function found");
--- src/lang_core/interp.rs
+++ src/lang_core/interp.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub enum VarValues {
+    Nil,
     Str(String),
     Num(f64),
 }
@@ -12,6 +13,9 @@ pub enum VarValues {
 impl ToString for VarValues {
     fn to_string(&self) -> String {
         match self {
+            VarValues::Nil => {
+                String::new()
+            }
             VarValues::Str(s) => {
                 s.to_owned()
             },
@@ -22,6 +26,17 @@ impl ToString for VarValues {
     }
 }
 
+impl From<&VarValues> for bool {
+    fn from(v: &VarValues) -> Self {
+        match v {
+            VarValues::Nil => false,
+            VarValues::Str(s) => !s.is_empty() && s != "0",
+            VarValues::Num(v) => *v != 0.0,
+        }
+    }
+}
+
+// todo: add actual garbage collector
 type Gc<T> = Rc<T>;
 
 pub struct Context {
@@ -37,23 +52,62 @@ impl Context {
         }
     }
     pub fn interpret(&mut self, prog: &Program) {
-        for instruction in &prog.instructions {
-            match instruction {
-                Instruction::PUSH(s) => {
-                    self.stack.push(Rc::new(RefCell::new(VarValues::Str(s.to_owned()))));
+        let mut inst = 0;
+        loop {
+            println!("stack: {:?}", self.stack);
+            println!("instr: {}, {:?}", inst, prog.instructions[inst]);
+            match &prog.instructions[inst] {
+                Instruction::PUSHSTR(s) => {
+                    self.stack.push(Gc::new(RefCell::new(VarValues::Str(s.to_owned()))));
+                }
+                Instruction::PUSHNIL => {
+                    self.stack.push(Gc::new(RefCell::new(VarValues::Nil)));
+                }
+                Instruction::IFFALSE(i) => {
+                    let test: bool = (&*self.stack.pop().unwrap().borrow()).into();
+                    if !test {
+                        inst = *i;
+                        continue;
+                    }
+                }
+                Instruction::GOTO(i) => {
+                    inst = *i;
+                    continue;
                 }
                 Instruction::CONCAT(n) => {
                     let n = *n;
                     if n >= 2 {
-                        let strings = self.stack.split_off(self.stack.len() - n).iter()
-                            .map(|v| v.borrow().to_string())
+                        let mut values = self.stack.split_off(self.stack.len() - n)
+                            .into_iter()
+                            .filter(|v| {
+                                match &*v.borrow() {
+                                    VarValues::Nil => false,
+                                    _ => true
+                                }
+                            })
                             .collect::<Vec<_>>();
-                        let string_len = strings.iter().map(|s| s.len()).sum();
-                        let mut new_string = String::with_capacity(string_len);
-                        for s in strings {
-                            new_string.push_str(&s);
+                        match values.len() {
+                            0 => {
+                                // only nil values found
+                                self.stack.push(Gc::new(RefCell::new(VarValues::Nil)));
+                            }
+                            1 => {
+                                // nothing else to concat with
+                                self.stack.push(values.pop().unwrap());
+                            }
+                            _ => {
+                                // multiple items, need to convert to strings first
+                                let strings = values.into_iter()
+                                    .map(|v| v.borrow().to_string())
+                                    .collect::<Vec<_>>();
+                                let string_len = strings.iter().map(|s| s.len()).sum();
+                                let mut new_string = String::with_capacity(string_len);
+                                for s in strings {
+                                    new_string.push_str(&s);
+                                }
+                                self.stack.push(Gc::new(RefCell::new(VarValues::Str(new_string))));
+                            }
                         }
-                        self.stack.push(Rc::new(RefCell::new(VarValues::Str(new_string))));
                     }
                 }
                 Instruction::SETVAR => {
@@ -69,6 +123,7 @@ impl Context {
                     break;
                 }
             }
+            inst += 1;
         }
     }
 }
