fa4f429f0a10785cc3cb41fd1c108d7f7e2281d6
Zakariah Nuccio <k1891842@kcl.ac.uk>
2021-02-09 06:53:48 +0000

    Revert "Add basic math and boolean operation functions"
    This reverts commit 7018ea4b

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -22,7 +22,16 @@ macro_rules! add_func {
 }
 
 pub fn register_builtins(vars: &mut HashMap<String, VarRefType>) {
+    add_func!(vars, boolean::not_func, "not");
     add_func!(vars, boolean::eq_func, "eq");
     add_func!(vars, boolean::ne_func, "ne");
+    add_func!(vars, boolean::lt_func, "lt");
+    add_func!(vars, boolean::gt_func, "gt");
+    add_func!(vars, boolean::le_func, "le");
+    add_func!(vars, boolean::ge_func, "ge");
     add_func!(vars, math::add_func, "add");
+    add_func!(vars, math::sub_func, "sub");
+    add_func!(vars, math::mul_func, "mul");
+    add_func!(vars, math::fdiv_func, "fdiv");
+    add_func!(vars, math::mod_func, "mod");
 }
